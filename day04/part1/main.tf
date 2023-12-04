3448640dda7a39e4039bf88ea93ec8758330336a
Gareth Somerville <gareth.somerville@infinityworks.com>
2023-12-04 10:00:14 +0000

    Day 4 part 1

--- day04/part1/main.tf
+++ day04/part1/main.tf
@@ -1,6 +1,14 @@
 locals {
+  inputFile      = file("${path.module}/../input")
+  input          = split("\n", local.inputFile)
+  without_game_number = [for line in local.input : split(":", line)[1]]
+  games = [for game in local.without_game_number : split("|", game)]
+  parsed = [for game in local.games : [for half in game: toset(regexall("[0-9]+", half))]]
+  winning_numbers = [for game in local.parsed : [for candidate in game[1]: candidate if contains(game[0], candidate)]]
+  winning_counts = [for numbers in local.winning_numbers : length(numbers)]
+  scores = [for count in local.winning_counts : count == 0 ? 0 : pow(2, count - 1)]
 }
 
 output "result" {
-  value = 0
+  value = sum(local.scores)
 }
--- main.tf
+++ main.tf
@@ -6,13 +6,9 @@ module "day02" { source = "./day02" }
 output "day02_part1" { value = module.day02.part1 }
 output "day02_part2" { value = module.day02.part2 }
 
-module "day03" { source = "./day03" }
-output "day03_part1" { value = module.day03.part1 }
-output "day03_part2" { value = module.day03.part2 }
-
-#module "day04" { source = "./day04" }
-#output "day04_part1" { value = module.day04.part1 }
-#output "day04_part2" { value = module.day04.part2 }
+module "day04" { source = "./day04" }
+output "day04_part1" { value = module.day04.part1 }
+output "day04_part2" { value = module.day04.part2 }
 
 #module "day05" { source = "./day05" }
 #output "day05_part1" { value = module.day05.part1 }
